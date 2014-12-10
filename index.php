cbbd47e3546d0f5f33992d8b138b2a35809b171f
whatXianheCodes <xianhehuang@gmail.com>
2014-12-10 01:50:19 -0800

    first version to get the site working - need alot of fixes

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Home</title>
+  <!-- Latest compiled and minified CSS -->
+  	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+	<div id="coming-soon">
+		<img src="Coming-Soon.jpg">
+	</div>
+	<div class = "logo-container">
+		<div class ="container">
+			<a href="https://www.facebook.com/xianhe.huang">
+				<img class="logo" src="FB-f-Logo__blue_144.png">
+			</a>
+			<a href="https://github.com/whatXianheCodes">
+				<img class="logo" src="GitHub-Mark-120px-plus.png">
+			</a>
+			<a href="https://www.linkedin.com/pub/xianhe-huang/5a/64a/91b">
+				<img class="logo" src="LinkedIn-InBug-2CRev.png">
+			</a>
+		</div>
+	</div>
+	<div id ="powered-by">
+		Powered By: Xianhe Huang
+	</div>
+	<!--<script type="text/javascript" src="http://www.skypeassets.com/i/scom/js/skype-uri.js"></script>
+		<div id="SkypeButton_Call_cheeeeese193_1">
+		  <script type="text/javascript">
+		    Skype.ui({
+		      "name": "chat",
+		      "element": "SkypeButton_Call_cheeeeese193_1",
+		      "participants": ["cheeeeese193"],
+		      "imageSize": 32
+		    });
+  	</script>
+  	<a href="https://twitter.com/xianhehuang" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @xianhehuang</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+</div> -->
+</body>
+</html>
